ad71f41dd19c1b7e33c6a9d60b4d058db74ce6ec
Sergey <s3gaddtv@gmail.com>
2020-09-26 05:56:02 +0300

    fix: for checklist

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const config = (env = {}) => {
       new HtmlWebpackPlugin({
         template: 'public/index.html',
         filename: './index.html',
-        // favicon: './src/assets/icon/favicon.ico',
+        favicon: './src/assets/icon/favicon.ico',
       }),
     ];
 
